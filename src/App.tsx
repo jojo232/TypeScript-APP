777716caada7e620d8c02dc1f8cd35c22384c21b
yuya <65487059+jojo232@users.noreply.github.com>
2023-04-25 10:20:44 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import theme from "./theme/theme";
 import { Router } from "./router/Router";
 
+// ChakraProviderは、Chakra UIというUIライブラリのコンポーネントを使用するためのプロバイダーであり、
+// themeプロパティによって、アプリケーション全体のテーマを設定することができます。
+
+
+// export defaultは、モジュールからデフォルトのエクスポートを提供するための構文。
+// この構文を使用すると、モジュールが1つの値または関数をエクスポートできるようになります。
+// 他のモジュールからは、デフォルトのエクスポートは単一の変数としてインポートする
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
@@ -14,3 +21,4 @@ export default function App() {
   );
 }
 // テーマを渡してあげてtheme={theme}
+
