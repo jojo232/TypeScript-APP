882b4b8d0e1e3a4953576bbe4cd7939c2581ec69
yuya <65487059+jojo232@users.noreply.github.com>
2023-04-25 10:35:33 +0900

    Update HomeRoutes.tsx

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -3,6 +3,9 @@ import { Setting } from "../components/pages/Setting";
 import { Home } from "../components/pages/Home";
 import { Page404 } from "../components/pages/Page404";
 
+
+// このコードは、アプリケーションのルーティングに使用する homeRoutes という配列を定義しています。
+// homeRoutes 配列の各要素は、アプリケーション内でアクセスできる URL のパスを示し、そのパスに対応するコンポーネントを表示します。
 export const homeRoutes = [
   {
     path: "/",
@@ -25,3 +28,6 @@ export const homeRoutes = [
     children: <Page404 />
   }
 ];
+
+// 例えば、/user_management という URL にアクセスした場合、<UserManagement /> コンポーネントが表示されます。
+// また、path: "*" という要素は、アプリケーション内で定義されていない URL にアクセスした場合に表示する 404 エラーページのコンポーネントを示しています。
