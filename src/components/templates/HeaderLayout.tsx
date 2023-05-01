6826abde0e6b261217cea509a2257828d1d2ce57
Yuya-Sasaki <65487059+jojo232@users.noreply.github.com>
2023-05-01 22:56:30 +0900

    Update HeaderLayout.tsx

--- src/components/templates/HeaderLayout.tsx
+++ src/components/templates/HeaderLayout.tsx
@@ -4,6 +4,11 @@ import { Header } from "../organisms/layout/Header";
 type Props = {
   children: ReactNode;
 };
+// このコードはpropsという型を定義しています。
+//　この型はchildrenという名前のプロパティを持ち、そのプロパティの値はReactNode型であると定義しています。
+// ReactNode型は、Reactでレンダリング可能なすべての要素を表します。
+// このように型を定義することで、Reactコンポーネントで受け取るpropsの型を明確にし、間違った型の値が渡されることを防ぐことができます。
+
 
 export const HeaderLayout: VFC<Props> = memo((props) => {
   const { children } = props;
