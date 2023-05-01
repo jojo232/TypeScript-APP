2a108a95ecc3b12020adc49bc0c5f0f789d1c137
Yuya-Sasaki <65487059+jojo232@users.noreply.github.com>
2023-05-01 23:02:36 +0900

    Update HeaderLayout.tsx

--- src/components/templates/HeaderLayout.tsx
+++ src/components/templates/HeaderLayout.tsx
@@ -1,6 +1,9 @@
 import { memo, VFC, ReactNode } from "react";
 import { Header } from "../organisms/layout/Header";
 
+// memo、VFCはReactコンポーネントの最適化のために使用されるものです。
+// HeaderLayoutコンポーネントは、propsとしてchildrenを受け取り、Headerコンポーネントとchildrenを返します。
+
 type Props = {
   children: ReactNode;
 };
@@ -19,3 +22,9 @@ export const HeaderLayout: VFC<Props> = memo((props) => {
     </>
   );
 });
+
+// このコードは、Reactの関数コンポーネント HeaderLayout を定義しています。 
+// HeaderLayout は、Propsとして children を受け取り、Header コンポーネントを先頭に置いて、その下に children を表示する役割を持っています。
+// また、VFC<Props> の形式で関数コンポーネントの型を定義しています。 
+// VFC は、"Void Function Component" の略で、戻り値がなく、Propsがある関数コンポーネントの型を表します。
+// この場合、 Props の型を指定しています。 Props の型は、children プロパティを持つ ReactNode 型のオブジェクトです。
